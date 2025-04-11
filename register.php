11bbc31a45318b8f6bcac0377294ace7f4e49f78
tchamouza <138805259+tchamouza@users.noreply.github.com>
2025-04-11 00:06:29 +0000

    Update register.php

--- register.php
+++ register.php
@@ -86,7 +86,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
 
     <!-- custom css file link  -->
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="Css/style.css">
 
 </head>
 
