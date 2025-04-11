c22feb6b6ba91b31047b72a8e23b9cb18534446e
Darrylwin <logossoudarryl20@gmail.com>
2025-04-11 09:16:36 +0000

    Refactor file structure and update links for consistency

--- register.php
+++ register.php
@@ -86,104 +86,105 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
 
     <!-- custom css file link  -->
-    <link rel="stylesheet" href="CSS/style.css">
+    <link rel="stylesheet" href="src/assets/css/style.css">
 
 </head>
 
 <body>
 
-    <header class="header">
+<header class="header">
 
-        <section class="flex">
+    <section class="flex">
 
-            <a href="home.html" class="logo">Nunya</a>
+        <a href="src/views/home.php" class="logo">Nunya</a>
 
-            <form action="search.html" method="post" class="search-form">
-                <input type="text" name="search_box" required placeholder="rechercher les cours..." maxlength="100">
-                <button type="submit" class="fas fa-search"></button>
-            </form>
+        <form action="search.html" method="post" class="search-form">
+            <input type="text" name="search_box" required placeholder="rechercher les cours..." maxlength="100">
+            <button type="submit" class="fas fa-search"></button>
+        </form>
 
-            <div class="icons">
-                <div id="menu-btn" class="fas fa-bars"></div>
-                <div id="search-btn" class="fas fa-search"></div>
-                <div id="user-btn" class="fas fa-user"></div>
-                <div id="toggle-btn" class="fas fa-sun"></div>
-            </div>
-
-            <div class="profile">
-                <img src="images/pic-1.jpg" class="image" alt="">
-                <h3 class="name">Abalo Kossi</h3>
-                <p class="role">Etudiant</p>
-                <a href="profile.html" class="btn">Voir Profile</a>
-                <div class="flex-btn">
-                    <a href="login.html" class="option-btn">Connexion</a>
-                    <a href="register.html" class="option-btn">Inscription</a>
-                </div>
-            </div>
-
-        </section>
-
-    </header>
-
-    <div class="side-bar">
-
-        <div id="close-btn">
-            <i class="fas fa-times"></i>
+        <div class="icons">
+            <div id="menu-btn" class="fas fa-bars"></div>
+            <div id="search-btn" class="fas fa-search"></div>
+            <div id="user-btn" class="fas fa-user"></div>
+            <div id="toggle-btn" class="fas fa-sun"></div>
         </div>
 
         <div class="profile">
-            <img src="images/pic-1.jpg" class="image" alt="">
+            <img src="src/assets/images/pic-1.jpg" class="image" alt="">
             <h3 class="name">Abalo Kossi</h3>
             <p class="role">Etudiant</p>
-            <a href="profile.html" class="btn">Voir Profile</a>
+            <a href="src/views/profile.html" class="btn">Voir Profile</a>
+            <div class="flex-btn">
+                <a href="src/views/auth/login.html" class="option-btn">Connexion</a>
+                <a href="src/views/auth/register.html" class="option-btn">Inscription</a>
+            </div>
         </div>
-
-        <nav class="navbar">
-            <a href="home.html"><i class="fas fa-home"></i><span>Accueil</span></a>
-            <a href="about.html"><i class="fas fa-question"></i><span>A Propos</span></a>
-            <a href="courses.html"><i class="fas fa-graduation-cap"></i><span>cours</span></a>
-            <a href="teachers.html"><i class="fas fa-chalkboard-user"></i><span>Enseignants</span></a>
-            <a href="contact.html"><i class="fas fa-headset"></i><span>Contactez-nous</span></a>
-        </nav>
-
-    </div>
-
-    <section class="form-container">
-
-        <form action="" method="post" enctype="multipart/form-data">
-            <h3>Inscription</h3>
-            <?php
-            if (!empty($error)) {
-                foreach ($error as $e) {
-                    echo "<p style='color:red;text-align:center'>$e</p>";
-                }
-            } elseif (!empty($success)) {
-                echo "<p style='color:green;text-align:center'>$success</p>";
-            }
-            ?>
-            <p>Nom <span>*</span></p>
-            <input type="text" name="name" placeholder="Entrez votre nom" required maxlength="50" class="box">
-            <p>Email <span>*</span></p>
-            <input type="email" name="email" placeholder="Entrez votre email" required maxlength="50" class="box">
-            <p>Mot de passe <span>*</span></p>
-            <input type="password" name="pass" placeholder="Entrez le mot de passe" required maxlength="20" class="box">
-            <p>Confirmer le mot de passe <span>*</span></p>
-            <input type="password" name="c_pass" placeholder="Confirmer le mot de passe" required maxlength="20" class="box">
-            <p>Photo <span>*</span></p>
-            <input type="file" name='image' accept="image/*" required class="box">
-            <input type="submit" value="S'inscrire" name="submit" class="btn">
-        </form>
 
     </section>
 
-    <footer class="footer">
+</header>
 
-        &copy; copyright @ 2025 by <span>Code Reapers | MBH 25</span> | all rights reserved
+<div class="side-bar">
 
-    </footer>
+    <div id="close-btn">
+        <i class="fas fa-times"></i>
+    </div>
 
-    <!-- custom js file link  -->
-    <script src="js/script.js"></script>
+    <div class="profile">
+        <img src="src/assets/images/pic-1.jpg" class="image" alt="">
+        <h3 class="name">Abalo Kossi</h3>
+        <p class="role">Etudiant</p>
+        <a href="src/views/profile.html" class="btn">Voir Profile</a>
+    </div>
+
+    <nav class="navbar">
+        <a href="src/views/home.php"><i class="fas fa-home"></i><span>Accueil</span></a>
+        <a href="src/views/about.html"><i class="fas fa-question"></i><span>A Propos</span></a>
+        <a href="src/views/courses.html"><i class="fas fa-graduation-cap"></i><span>cours</span></a>
+        <a href="src/views/teachers.html"><i class="fas fa-chalkboard-user"></i><span>Enseignants</span></a>
+        <a href="src/views/contact.html"><i class="fas fa-headset"></i><span>Contactez-nous</span></a>
+    </nav>
+
+</div>
+
+<section class="form-container">
+
+    <form action="" method="post" enctype="multipart/form-data">
+        <h3>Inscription</h3>
+        <?php
+        if (!empty($error)) {
+            foreach ($error as $e) {
+                echo "<p style='color:red;text-align:center'>$e</p>";
+            }
+        } elseif (!empty($success)) {
+            echo "<p style='color:green;text-align:center'>$success</p>";
+        }
+        ?>
+        <p>Nom <span>*</span></p>
+        <input type="text" name="name" placeholder="Entrez votre nom" required maxlength="50" class="box">
+        <p>Email <span>*</span></p>
+        <input type="email" name="email" placeholder="Entrez votre email" required maxlength="50" class="box">
+        <p>Mot de passe <span>*</span></p>
+        <input type="password" name="pass" placeholder="Entrez le mot de passe" required maxlength="20" class="box">
+        <p>Confirmer le mot de passe <span>*</span></p>
+        <input type="password" name="c_pass" placeholder="Confirmer le mot de passe" required maxlength="20"
+               class="box">
+        <p>Photo <span>*</span></p>
+        <input type="file" name='image' accept="image/*" required class="box">
+        <input type="submit" value="S'inscrire" name="submit" class="btn">
+    </form>
+
+</section>
+
+<footer class="footer">
+
+    &copy; copyright @ 2025 by <span>Code Reapers | MBH 25</span> | all rights reserved
+
+</footer>
+
+<!-- custom js file link  -->
+<script src="src/assets/js/script.js"></script>
 
 </body>
 
